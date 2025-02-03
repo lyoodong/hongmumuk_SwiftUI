f9c5c976ff88e0929fc5a8c91eb049b8e5d358d0
30 <127213160+30isdead@users.noreply.github.com>
2025-02-03 22:23:25 +0900

    [Chore] SwiftLint 적용 오류 수정 (#10)
    * 🔧 SwiftLint 적용 오류 수정
    
    * ✨swiftformat 추가 및 swiftLint disabled rule 추가 (#12)
    
    ---------
    
    Co-authored-by: DongWan <115209527+lyoodong@users.noreply.github.com>

--- hongmumuk_SwiftUITests/hongmumuk_SwiftUITests.swift
+++ hongmumuk_SwiftUITests/hongmumuk_SwiftUITests.swift
@@ -5,11 +5,10 @@
 //  Created by Dongwan Ryoo on 1/19/25.
 //
 
-import XCTest
 @testable import Hongmumuk_SwiftUI
+import XCTest
 
 final class Hongmumuk_SwiftUITests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -28,9 +27,8 @@ final class Hongmumuk_SwiftUITests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
