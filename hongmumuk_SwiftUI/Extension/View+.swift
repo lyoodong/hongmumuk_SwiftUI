c9439f118631fc27734ea92a9a204b24b057dedb
30 <1224tpdud@gmail.com>
2025-02-03 17:05:12 +0900

    ♻️ for문에서 Reduce로 변경

--- hongmumuk_SwiftUI/Extension/View+.swift
+++ hongmumuk_SwiftUI/Extension/View+.swift
@@ -9,17 +9,14 @@ import SwiftUI
 
 extension View {
     func applyShadows(_ style: ShadowStyle) -> some View {
-        var modifiedView = AnyView(self)
-        for shadow in style.shadows {
-            modifiedView = AnyView(
-                modifiedView.shadow(
-                    color: shadow.color,
-                    radius: shadow.radius,
-                    x: shadow.x,
-                    y: shadow.y
-                )
-            )
+        style.shadows.reduce(AnyView(self)) { view, shadow in
+            AnyView(view.shadow(
+                color: shadow.color,
+                radius: shadow.radius,
+                x: shadow.x,
+                y: shadow.y
+            ))
         }
-        return modifiedView
     }
 }
+
