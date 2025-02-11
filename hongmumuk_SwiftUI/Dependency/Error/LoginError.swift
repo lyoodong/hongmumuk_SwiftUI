63d82dc423f598f4863b33a296d9b51b1486de1f
DongWan <115209527+lyoodong@users.noreply.github.com>
2025-02-11 21:48:08 +0900

    [Feat] authClient 추가 (#17)
    * ✨baseurl 환경 변수 추가
    
    * ✨ AuthClient 추가

---
+++ hongmumuk_SwiftUI/Dependency/Error/LoginError.swift
@@ -0,0 +1,13 @@
+//
+//  LoginError.swift
+//  hongmumuk_SwiftUI
+//
+//  Created by Dongwan Ryoo on 2/10/25.
+//
+
+import Foundation
+
+enum LoginError: String, Error {
+    case unKnown
+    case invalid
+}
---
+++ hongmumuk_SwiftUI/Dependency/Model/LoginModel.swift
@@ -0,0 +1,13 @@
+//
+//  LoginModel.swift
+//  hongmumuk_SwiftUI
+//
+//  Created by Dongwan Ryoo on 2/10/25.
+//
+
+import Foundation
+
+struct LoginModel: Codable {
+    let email: String
+    let password: String
+}
