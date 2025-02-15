7dbf623df5e5233ed3ac87e3b6d109130cff5db0
lyoodong <dwryu3079@naver.com>
2025-02-15 21:22:49 +0900

    💄searchView 추가

--- hongmumuk_SwiftUI/Feature/Search/SearchView.swift
+++ hongmumuk_SwiftUI/Feature/Search/SearchView.swift
@@ -5,4 +5,41 @@
 //  Created by Dongwan Ryoo on 2/14/25.
 //
 
-import Foundation
+import ComposableArchitecture
+import SwiftUI
+
+// MARK: - Store List View
+
+struct SearchView: View {
+    private let store: StoreOf<SearchFeature>
+    @ObservedObject var viewStore: ViewStoreOf<SearchFeature>
+    
+    init(store: StoreOf<SearchFeature>) {
+        self.store = store
+        viewStore = ViewStore(store, observe: { $0 })
+    }
+    
+    var body: some View {
+        NavigationView {
+            VStack(spacing: 0) {
+                SearchHeaderView(viewStore: viewStore)
+                RecentSearchView(viewStore: viewStore)
+                SearchListView(viewStore: viewStore)
+            }
+        }
+        .onAppear {
+            viewStore.send(.onAppear)
+        }
+        .navigationDestination(
+            isPresented: viewStore.binding(
+                get: { $0.activeScreen != .none },
+                send: .onDismiss
+            )
+        ) {
+            let screen = viewStore.activeScreen
+            if case let .restrauntDetail(id) = screen {
+                // 상세화면 전환
+            }
+        }
+    }
+}
