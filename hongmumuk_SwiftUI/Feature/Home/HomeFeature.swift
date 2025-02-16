782581bd9f4a040fb8ad36d3ebeaa14a8b7d46a5
lyoodong <dwryu3079@naver.com>
2025-02-16 15:29:13 +0900

    ♻️ UI 성능 최적화

--- hongmumuk_SwiftUI/Feature/Home/HomeFeature.swift
+++ hongmumuk_SwiftUI/Feature/Home/HomeFeature.swift
@@ -15,7 +15,7 @@ struct HomeFeature: Reducer {
         case random
         case cateogryList(Category)
     }
-    
+
     struct State: Equatable {
         var categories: [Category] = []
         var activeScreen: ActiveScreen = .none
@@ -24,49 +24,33 @@ struct HomeFeature: Reducer {
         var innerRadius: CGFloat = 0
         var chordLength: CGFloat = 0
         var startAngle: Double? = nil
-        var midAngle: Double? = nil
-        
+
         var sliceAngle: Double {
             360.0 / Double(categories.count)
         }
-        
+
         var gapAngleDeg: Double {
             let gapAngleRad = 2 * asin(chordLength / (2 * outerRadius))
             return Double(gapAngleRad) * 180 / .pi
         }
-        
+
         var effectiveAngle: Double {
             sliceAngle - gapAngleDeg
         }
-        
+
         var midRadius: CGFloat {
             (innerRadius + outerRadius) / 2
         }
-        
+
         var diameter: CGFloat {
             outerRadius * 2
         }
-        
+
         var centerPosition: CGPoint {
             CGPoint(x: outerRadius, y: outerRadius)
         }
-    
-        func calculatePosition(_ index: Int) -> CGPoint {
-            let start = sliceAngle * Double(index) + rotation
-            let end = start + sliceAngle
-            let angle = (start + end) / 2
-            let angleRad = angle * .pi / 180
-            let x = outerRadius + midRadius * cos(angleRad)
-            let y = outerRadius + midRadius * sin(angleRad)
-
-            return CGPoint(x: x, y: y)
-        }
-        
-        func rotationAngle(_ index: Int) -> Angle {
-            return .degrees((sliceAngle * Double(index)) + rotation)
-        }
     }
-    
+
     enum Action: Equatable {
         case onAppear
         case onDismiss
@@ -76,7 +60,7 @@ struct HomeFeature: Reducer {
         case dragChanged(DragGesture.Value)
         case dragEnded
     }
-    
+
     func reduce(into state: inout State, action: Action) -> Effect<Action> {
         switch action {
         case .onAppear:
@@ -85,36 +69,41 @@ struct HomeFeature: Reducer {
             state.rotation = -state.sliceAngle * 3
             state.outerRadius = UIScreen.main.bounds.width / 2 - 24
             state.innerRadius = 76
-            state.chordLength = 8
-            
+
             return .none
         case .onDismiss:
             state.activeScreen = .none
             return .none
         case let .categoryItemTapped(category):
             state.activeScreen = .cateogryList(category)
-            
+
             return .none
         case .randomButtonTapped:
             state.activeScreen = .random
-            
+
             return .none
         case .searchButtonTapped:
             state.activeScreen = .search
-            
+
             return .none
         case let .dragChanged(value):
             let center = state.centerPosition
             let currentAngle = atan2(value.location.y - center.y, value.location.x - center.x) * 180 / .pi
-            state.startAngle = state.startAngle ?? (currentAngle - state.rotation)
-            state.rotation = currentAngle - state.startAngle!
 
+            if let lastAngle = state.startAngle {
+                var delta = currentAngle - lastAngle
+                // 180° 이상 차이나면 보정
+                if delta > 180 { delta -= 360 }
+                if delta < -180 { delta += 360 }
+                state.rotation += delta
+            }
+            
+            state.startAngle = currentAngle
             return .none
         case .dragEnded:
             state.startAngle = nil
             let snappedRotation = round(state.rotation / state.sliceAngle) * state.sliceAngle
             state.rotation = snappedRotation
-            
             return .none
         }
     }
