d2dc9efb132663893b6f5cceb623fc5d074aa5ac
lyoodong <dwryu3079@naver.com>
2025-02-17 18:38:09 +0900

    💄최근 검색어 UI 확정에 따른 변경 사항

--- hongmumuk_SwiftUI/Feature/Search/SearchRecentView.swift
+++ hongmumuk_SwiftUI/Feature/Search/SearchRecentView.swift
@@ -12,67 +12,80 @@ struct RecentSearchView: View {
     @ObservedObject var viewStore: ViewStoreOf<SearchFeature>
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 8) {
-            HStack {
-                Text("최근 검색 결과")
-                    .font(.system(size: 16, weight: .bold))
-                    .foregroundColor(.black)
-                
-                Spacer()
-                
-                Button(action: {
-                    viewStore.send(.recentSearchAllClearButtonTapped)
-                }) {
-                    Text("전체 삭제")
-                        .font(.system(size: 14))
-                        .foregroundColor(.blue)
-                }
-                .padding(.leading, 8)
-            }
+        VStack(alignment: .leading) {
+            recentSearchTextView
+            if viewStore.recentSearchList.isEmpty { recentSearchEmptyTextView }
+            else { recentSearchItemView }
+        }
+        .padding(.horizontal, 20)
+    }
+    
+    private var recentSearchTextView: some View {
+        HStack {
+            Text("최근 검색 결과")
+                .fontStyle(Fonts.body2SemiBold)
+                .foregroundColor(Colors.GrayScale.grayscal45)
+                .padding(.top, 24)
             
-            if viewStore.recentSearchList.isEmpty {
-                HStack {
-                    Spacer()
-                    Text("최근 검색 결과가 없습니다.")
-                        .font(.system(size: 14))
-                        .foregroundColor(.gray)
-                    Spacer()
-                }
-                .frame(height: 16)
-            } else {
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack(spacing: 8) {
-                        ForEach(viewStore.recentSearchList, id: \.self) { searchText in
-                            HStack(spacing: 4) {
-                                Text(searchText)
-                                    .font(.system(size: 14))
-                                    .foregroundColor(.black)
-                                    .padding(.vertical, 6)
-                                    .padding(.horizontal, 12)
-                                
-                                Button(action: {
-                                    viewStore.send(.recentSearchClearButtonTapped(searchText))
-                                }) {
-                                    Image(systemName: "xmark.circle.fill")
-                                        .resizable()
-                                        .frame(width: 16, height: 16)
-                                        .foregroundColor(.gray)
-                                }
-                            }
-                            .background(recentSearchItemBackgroundView)
-                            .onTapGesture {
-                                viewStore.send(.recentSearchTapped(searchText))
-                            }
+            Spacer()
+            
+            Button(action: {
+                viewStore.send(.recentSearchAllClearButtonTapped)
+            }) {
+                Text("전체 삭제")
+                    .fontStyle(Fonts.body2Medium)
+                    .foregroundColor(Colors.GrayScale.grayscale30)
+                    .padding(.top, 26)
+            }
+            .padding(.leading, 8)
+        }
+    }
+    
+    private var recentSearchEmptyTextView: some View {
+        HStack {
+            Spacer()
+            Text("최근 검색 결과가 없습니다.")
+                .font(.system(size: 14))
+                .foregroundColor(.gray)
+            Spacer()
+        }
+        .frame(height: 16)
+        .padding(.top, 24)
+    }
+    
+    private var recentSearchItemView: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 8) {
+                ForEach(viewStore.recentSearchList, id: \.self) { searchText in
+                    HStack(spacing: 4) {
+                        Text(searchText)
+                            .fontStyle(Fonts.body1Medium)
+                            .foregroundColor(Colors.GrayScale.grayscale70)
+                            .padding(.vertical, 6)
+                        
+                        Button(action: {
+                            viewStore.send(.recentSearchClearButtonTapped(searchText))
+                        }) {
+                            Image("xIcon")
+                                .resizable()
+                                .frame(width: 16, height: 16)
+                                .foregroundColor(.gray)
                         }
+                    }
+                    .padding(.horizontal, 12)
+                    .background(recentSearchItemBackgroundView)
+                    .onTapGesture {
+                        viewStore.send(.recentSearchTapped(searchText))
                     }
                 }
             }
         }
-        .padding(.horizontal, 20)
+        .padding(.top, 12)
     }
     
     private var recentSearchItemBackgroundView: some View {
         RoundedRectangle(cornerRadius: 20)
             .fill(Colors.GrayScale.grayscale5)
+            .stroke(Colors.Border.normal)
     }
 }
